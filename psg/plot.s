006f2cd4a6ec2f4f1f0537ce426107c3e603f112
root <root@localhost.idc.pplive.cn>
2013-07-19 16:55:09 +0800

    add cgi-bin

--- psg/plot.s
+++ psg/plot.s
@@ -1,5 +1,5 @@
 set term jpeg
-set output "/tmp/test.jpg"
+set output "tmp/test.jpg"
 set timefmt "%H:%M:%S"
 set xdata time
-plot "/tmp/test" using 1:2 with lines
+plot "tmp/test.sar" using 1:2 with lines
